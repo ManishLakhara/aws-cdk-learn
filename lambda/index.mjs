842e68da667445cfa304728e5a2d046f36e88f49
Manish Lakhara <manishlakhara60402001@gmail.com>
2023-06-06 18:47:54 +0530

    Lambda work done

---
+++ lambda/index.mjs
@@ -0,0 +1,36 @@
+import AWS from "aws-sdk";
+const apiGateway = new AWS.ApiGatewayManagementApi({
+  endpoint: "l",
+  apiVersion: "2018-11-29",
+  region: "us-east-1",
+});
+export const handler = async (event) => {
+  // TODO implement
+  console.log(AWS.VERSION);
+  const { requestContext, body } = event;
+  const connectionId = requestContext.connectionId;
+  const routeKey = requestContext.routeKey;
+
+  if (routeKey === "$connect") {
+    // Handle new connection
+    console.log(`New connection: ${connectionId}`);
+  } else if (routeKey === "$disconnect") {
+    // Handle disconnection
+    console.log(`Disconnected: ${connectionId}`);
+  } else if (routeKey === "sendMessage") {
+    // Handle incoming messages
+    console.log(`Received message from ${connectionId}: ${body}`);
+
+    await apiGateway
+      .postToConnection({
+        ConnectionId: connectionId,
+        Data: `Hello from lambda`,
+      })
+      .promise();
+  }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+  return response;
+};
--- lib/aws-cdk-1-stack.ts
+++ lib/aws-cdk-1-stack.ts
@@ -1,16 +1,38 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import {
+  aws_s3 as s3,
+  aws_lambda as lambda,
+  aws_apigateway as apigateway,
+} from "aws-cdk-lib";
+import { aws_iam as iam } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import path = require("path");
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdk1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdk1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //This is Code for lambda handling function.
+    const layer = new lambda.LayerVersion(this, "MyLayer-2", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "aws-sdk.zip")),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_18_X],
+      license: "Apache-2.0",
+      description: "A layer to test the L2 construct",
+    });
+    const myRole = new iam.Role(this, "lambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+    myRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    const fn = new lambda.Function(this, "My Function", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "..", "lambda")),
+      layers: [layer],
+    });
+    //This is Code for apigateway creating
   }
 }
